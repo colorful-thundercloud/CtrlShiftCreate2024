c4ddff227713a7dfcc2b1a5a7fe4a9fa3fe593aa
Cor Lapus <kirisakisarim@gmail.com>
2024-03-22 16:50:15 +0300

    Моя сцена - копия сцены Сергея
    скрипт начинает вырисовываться

--- Assets/Scripts/Kate/BigBrain.cs
+++ Assets/Scripts/Kate/BigBrain.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class BigBrain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject Field;
+    [SerializeField] GameObject Hand;
+    List<BasicCard> myCards = new List<BasicCard>();
+    List<BasicCard> playerCards = new List<BasicCard>();
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            EnemyTurn();
+        }
+    }
+
+    void EnemyTurn()
+    {
+        myCards = Field.GetComponent<Field>().GetCards(true);
+        playerCards = Field.GetComponent<Field>().GetCards(false);
+        Debug.Log("Enemy's Cards:");
+        foreach (BasicCard card in myCards)
+        {
+            Debug.Log("HP: " + card.HP + ", Damage: " + card.Damage);
+        }
+        //получение карт в руку
+        //если на поле все свободно, запустить растановку
+        //если есть баффы применить их
+        //если на поле есть свободное место и в руке есть юниты запустить скрипт постановки
+    }
+
+    void DoIUnit()//скрипт постановки
+    {
+        //оценка юнитов игрока
+        //оценка своих юнитов
+        //сравнение
+        //оценка юнитов в руке
+        //если сильно в пользу нас, и наши карты слабы ничего не делаем
+        //если все же мы слабее/равны, то ставим карту
+    }
+
+    void WhichCardSpawn()//если несколько карт для спавна
+    {
+        //мяу
+    }
+
+    void DoBaff()
+    {
+        //применяем баф
     }
 }
