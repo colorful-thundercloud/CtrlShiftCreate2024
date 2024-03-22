af97e03f3eabc3eeb1bd44ad98c78361e5a2fdd1
Stephan0404 <stephan906082@gmail.com>
2024-03-22 13:44:09 +0300

    basicUI

--- Assets/Scripts/mainMenuController.cs
+++ Assets/Scripts/mainMenuController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenuController : MonoBehaviour
 {
     public void Exit()
     {
         Application.Quit();
+    }
+    public void sceneController(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
     }
 }
