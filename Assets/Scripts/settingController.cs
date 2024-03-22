38336fd6c7e7f12d17de3cfae4173efe2e1d1ee0
Stephan0404 <stephan906082@gmail.com>
2024-03-22 12:24:53 +0300

    MainMenu

---
+++ Assets/Scripts/settingController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class settingController : MonoBehaviour
+{
+    public TMP_Dropdown screenExtension;
+    public void screenExtensionMenu()
+    {
+        if (screenExtension.value == 0)
+        {
+            Screen.SetResolution(720, 1080, true);
+            Debug.Log("Расширь свое расширение на 720*1080");
+        }
+        if (screenExtension.value == 1)
+        {
+            Screen.SetResolution(1920, 1080, true);
+            Debug.Log("Расширь свое расширение на 1920*1080");
+        }
+        if (screenExtension.value == 2)
+        {
+            Screen.SetResolution(2560, 1440, true);
+            Debug.Log("Расширь свое расширение на 2560*1440");
+        }
+       
+    }
+}
