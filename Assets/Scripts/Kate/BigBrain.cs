2d54ec9ebf6d18f58080008e263986f4a75d3744
Cor Lapus <kirisakisarim@gmail.com>
2024-03-24 00:32:32 +0300

    enumerator bigbrain

--- Assets/Scripts/Kate/BigBrain.cs
+++ Assets/Scripts/Kate/BigBrain.cs
@@ -13,12 +13,44 @@ public class BigBrain : MonoBehaviour
     List<Card> myCards = new();
     List<Card> myCardsOnBoard = new();
     List<Card> playerCards = new();
-    public void EnemyTurn()
+    public IEnumerator EnemyTurn()
     {
         List<Card> StartBoard = field.GetComponent<Field>().GetCards(true);
         playerCards = field.GetComponent<Field>().GetCards(false);
         myCards = hand.GetComponent<Hand>().GetCards();
+
         SpawnUnit(WhichCardsSpawnUnit(), 3 - myCardsOnBoard.Count);
+        List<Card> cardsToSpawn = WhichCardsSpawnUnit();
+        int HowMuch = 3 - myCardsOnBoard.Count;
+        if (HowMuch != 0)
+        {
+            if (cardsToSpawn.Count != 0)
+            {
+                for (int i = myCards.Count - 1; i >= 0; i--)
+                {
+                    if (HowMuch == 0) break;
+                    for (int j = 0; j < cardsToSpawn.Count; j++)
+                    {
+                        if (myCards[i] == cardsToSpawn[j])
+                        {
+                            while (myCards[i].gameObject.transform.position.y - field.GetComponent<Field>().GetEnemyField().position.y > 0.1f)
+                            {
+                                Vector3 direction = (field.GetComponent<Field>().GetEnemyField().position - myCards[i].gameObject.transform.position).normalized;
+                                myCards[i].gameObject.GetComponent<Rigidbody2D>().MovePosition(myCards[i].gameObject.transform.position + direction * Time.deltaTime);
+                                yield return null;
+                            }
+                            myCards[i].EnemyCast();
+                            myCards[i].OnMouseUp();
+                            field.GetComponent<Field>().addCard(myCards[i], true);
+                            cardsToSpawn.Remove(cardsToSpawn[j]);
+                            HowMuch--;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
         myCardsOnBoard = field.GetComponent<Field>().GetCards(true);
         DoBaff(WhichCardsSpawnBaff());
         Attack(StartBoard);
