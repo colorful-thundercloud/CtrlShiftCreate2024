8caa9896a3f2ccf2c8d4e7ec712ee3b467d78239
Stephan0404 <stephan906082@gmail.com>
2024-07-25 02:03:35 +0300

    Network seyying and lobby WIP

---
+++ Assets/Scripts/Network/Lobby.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour
+{
+    [SerializeField] TMP_InputField JoinMathInput;
+    [SerializeField] Button HostBtn;
+    [SerializeField] Button ClientBtn;   
+public void Host()
+    {
+        JoinMathInput.interactable = false;
+        HostBtn.interactable = false;
+        ClientBtn.interactable = false;
+    }
+
+   public void Join()
+    {
+        JoinMathInput.interactable = false;
+        HostBtn.interactable = false;
+        ClientBtn.interactable = false;
+    }
+}
