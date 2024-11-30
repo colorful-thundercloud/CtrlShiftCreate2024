d3e8722cd17c5ac48ad0d1b30db6dec9dffbea9f
Stephan0404 <stephan906082@gmail.com>
2024-11-30 13:21:12 +0300

    last update

---
+++ Assets/Scripts/Network/NetworkManagerLobby.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System.Linq;
+
+public class NetworkManagerLobby : NetworkBehaviour
+{
+    /*[Scene][SerializeField] private string menuScene = string.Empty;
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisConnected;
+
+
+    public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+
+    public override void OnStartClient()
+    {
+      
+    }*/
+}
